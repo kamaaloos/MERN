43189136f22763d115d78f1c63786ec1e2a63a62
kamaaloos <kamaaloos@gmail.com>
2020-03-11 17:00:01 +0200

    Add mongoDB and google oauth

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleCLientID : process.env.GOOGLE_CLIENT_ID,
+    googleCLientSecret : Process.env.GOOGLE_SECRET,
+    mongoURI: Process.env.MONGO_URI,
+    cookieKeys: process.env.COOKIE_KEYS
+    };
--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require("./services/passport");
+
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get("/", (req, res)=>{
-res.send({Hello: 'World'})
+app.use( cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKeys]
 })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+
+
 
 var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
-    console.log("Server has strated at port 3000");
+    console.log("Server has started at port 3000");
     
 });
